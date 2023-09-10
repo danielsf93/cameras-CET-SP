e07337ce91988ac37aa06a28d16c8fe2c8bab795
Daniel Silva Ferreira <danielsilva@usp.br>
2023-09-10 10:24:06 -0300

    Add files via upload

---
+++ cameras.php
@@ -0,0 +1,21 @@
+<?php
+            for ($cam = 1; $cam <= 50; $cam++) {
+                echo '<iframe width="700" height="405" src="http://player.cxtv.com.br/camcet.php?Cam=' . $cam . '" frameborder="0" allowfullscreen></iframe>';
+            }
+            ?>
+        </div>
+        <div id="tabs-2">
+            <!-- Câmeras de 51 a 100 -->
+            <?php
+            for ($cam = 51; $cam <= 100; $cam++) {
+                echo '<iframe width="700" height="405" src="http://player.cxtv.com.br/camcet.php?Cam=' . $cam . '" frameborder="0" allowfullscreen></iframe>';
+            }
+            ?>
+        </div>
+        <div id="tabs-3">
+            <!-- Câmeras de 101 a 150 -->
+            <?php
+            for ($cam = 101; $cam <= 150; $cam++) {
+                echo '<iframe width="700" height="405" src="http://player.cxtv.com.br/camcet.php?Cam=' . $cam . '" frameborder="0" allowfullscreen></iframe>';
+            }
+            ?>
